9ef707abdbc8afbf59196804460b37e7ccf9ca23
jlbuild <juliabuildbot@gmail.com>
2026-03-03 21:18:19 +0000

    FMM3D_jll build 2.1.0+0

--- src/FMM3D_jll.jl
+++ src/FMM3D_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("FMM3D")
-JLLWrappers.@generate_main_file("FMM3D", UUID("62c264cc-5bfb-59d7-84eb-c0e4dde7f66b"))
+JLLWrappers.@generate_main_file("FMM3D", Base.UUID("62c264cc-5bfb-59d7-84eb-c0e4dde7f66b"))
 end  # module FMM3D_jll
